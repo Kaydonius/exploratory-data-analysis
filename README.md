a7243f28a02e5ad0ecc39fe0ad1686a49fbf0fa0
Kaydonius <kaydoniusprogramming@gmail.com>
2025-06-13 20:15:19 -0700

    setting up venv

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # exploratory-data-analysis
+
+porfolio project # 1 - working a w/planets (i love outer space lol)
---
+++ planet_dataset.py
@@ -0,0 +1,6 @@
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("iamsouravbanerjee/planet-dataset")
+
+print("Path to dataset files:", path)
